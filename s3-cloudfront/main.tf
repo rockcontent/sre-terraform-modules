42aa311fdf17fd2a2cbf2aa61b1839c0a643c1e7
sre-rockcontent <117839006+sre-rockcontent@users.noreply.github.com>
2022-11-24 08:24:42 -0300

    Update main.tf

--- s3-cloudfront/main.tf
+++ s3-cloudfront/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "3.74.0"
+      version = "3.75.0"
     }
   }
 }
@@ -11,4 +11,9 @@ provider "aws" {
   region     = var.AWS_REGION
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY 
+}
+
+provider "aws" {
+  alias = "virginia"
+  region = "us-east-1"
 }
