b7384f1577cc5e9b2eea60386d8c20830a9cb8ba
franklin_sampaio <franklin.sampaio@grupomytec.com.br>
2022-12-07 09:14:56 -0300

    Added alb

---
+++ alb/alb.tf
@@ -0,0 +1,48 @@
+resource "aws_lb" "alb" {
+  name               = var.lb_name
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = aws_s3_bucket.lb_logs.bucket
+    prefix  = "test-lb"
+    enabled = true
+  }
+
+  tags = {
+    Environment = "production"
+  }
+}
+
+resource "aws_security_group" "lb_sg" {
+  name        = "${var.lb_name}-lb"
+  description = "Allows access from the ALB only"
+  vpc_id      = var.vpc
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+  }
+}
---
+++ alb/variables.tf
@@ -0,0 +1,13 @@
+#ALB
+
+variable "lb_name" {
+    type = string
+    default = ""
+}
+
+
+# VPC
+
+variable "vpc" {
+  type = string
+}
