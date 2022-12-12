fb7e2b9a38e15cf0b640a6f5728025697169eb13
André Chagas <andreabreuchagas@gmail.com>
2022-12-12 13:50:52 -0300

    fix

--- ecs-ec2/variable.tf
+++ ecs-ec2/variable.tf
@@ -5,15 +5,15 @@ variable "tags" {
 }
 
 variable "task_environment" {
-  type        = list
+  type        = list(any)
   description = "Task environment variables"
-  default     = []
+  default     = null
 }
 
 variable "task_secrets" {
-  type        = list
+  type        = list(any)
   description = "Task environment secrets"
-  default     = []
+  default     = null
 }
 
 
